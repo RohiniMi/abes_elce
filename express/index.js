9ac1d2235f9a04397832ffc8fc3539cb13b5edff
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-05-02 10:08:12 +0530

    crud

--- express/index.js
+++ express/index.js
@@ -1,9 +1,8 @@
 import express from "express";
 import rw from "./rw.js";
 const app = express();
-
 //get method
-app.get("/", async (req, res) => {
+app.get("/user", async (req, res) => {
     try {
         const data = await rw.readFile();
         res.status(data.status).json({ "data": JSON.parse(data.data), "message": data.message });
@@ -12,16 +11,34 @@ app.get("/", async (req, res) => {
     }
 });
 app.use(express.json()); //inbulit express middleware
-app.post("/", async(req, res) => {
+app.post("/user", async (req, res) => {
     try {
         const data = req.body;
-        console.log(data);
-        if(data) await rw.writeFile(data);
-        
+        if (data) await rw.writeFile(data);
         res.status(201).json({ "data": data });
     } catch (error) {
         res.status(500).json({ "Error": error });
     }
+})
+app.put("/user/:email", async (req, res) => {
+    const userToFind = req.params.email;
+    try {
+        const data = await rw.updateFile(req.body, userToFind);
+        res.status(data.status).json({ "message": data.message });
+    } catch (error) {
+        res.status(500).json({ "Error": error });
+    }
+})
+app.delete("/user/:email", async (req, res) => {
+    const userToDelete = req.params.email;
+    try {
+        const data = await rw.deleteUser(userToDelete);
+        console.log(data);
+        res.status(data.status).json({ "message": data.message });
+    } catch (error) {
+        res.status(500).json({ "Error": error });
+    }
+
 })
 const PORT = 8800;
 app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
--- express/rw.js
+++ express/rw.js
@@ -1,30 +1,76 @@
+import { log } from "console";
 import fs from "fs/promises";
-const readFile = async()=>{
+const readFile = async () => {
     let message = "";
     let status = 500;
     let data = [];
     try {
-        const fileData = await fs.readFile("./data.json","utf-8");
+        const fileData = await fs.readFile("./data.json", "utf-8");
         message = "File has been read successfully.(async)";
         status = 200;
-        data = data;    
+        data = fileData;
     } catch (error) {
-       message = "Read error";
-       status = 500;
-       data = error;    
+        message = "Read error";
+        status = 500;
+        data = error;
     }
-    return {data,status,message}
+    return { data, status, message }
 }
 const writeFile = async (data) => {
     try {
         let dataToSave = [];
-        const fileData = await readFile();//read
-        
-        
-        await fs.writeFile("./data.json", JSON.stringify(data));
+        const fileData = await readFile();//read  
+        console.log(fileData.data);
+
+        if (fileData) dataToSave = JSON.parse(fileData.data);  //if file not empty
+        if (!Array.isArray(dataToSave)) dataToSave = [dataToSave];
+        dataToSave.push(data);
+
+        await fs.writeFile("./data.json", JSON.stringify(dataToSave, null, 2));
         console.log("File has been written successfully.(async)");
     } catch (error) {
-        console.log("unable to write file.(async)",error);       
+        console.log("unable to write file.(async)", error);
     }
 }
-export default {readFile,writeFile};
+const updateFile = async (data, email) => {
+    let status = 500;
+    let message = "Updation Error";
+    try {
+        let dataToSave = [];
+        const fileData = await readFile();//read  
+        if (fileData) dataToSave = JSON.parse(fileData.data);
+        const userToUpdate = dataToSave.find((d) => d.email === email);
+        if (userToUpdate) {
+            userToUpdate.password = data.password;
+        } else {
+            status = 500;
+            message = "User doesn't exist."
+        }
+        await fs.writeFile("./data.json", JSON.stringify(dataToSave, null, 2));
+        status = 200;
+        message = "User updated successfully."
+
+    } catch (error) {
+        status = 500;
+        message = "Updation Error."
+    }
+    return { status, message };
+}
+const deleteUser = async (email) => {
+    let status = 500;
+    let message = "Deletion Error";
+    try {
+        let dataToSave = [];
+        const fileData = await readFile();//read  
+        if (fileData) dataToSave = JSON.parse(fileData.data);
+        dataToSave = dataToSave.filter((d) => d.email !== email);
+        console.log(dataToSave);
+        await fs.writeFile("./data.json", JSON.stringify(dataToSave, null, 2));
+        status = 200;
+        message = "deleted successfully."
+    } catch (error) {
+          message = error.message;
+    }
+    return {status,message};
+}
+export default { readFile, writeFile, updateFile, deleteUser };
